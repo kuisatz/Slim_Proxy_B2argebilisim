12629eaa86d21a79fefc599a8eeb58cd7c4ee41d
kuisatz <kuisatzhaderah@gmail.com>
2016-11-04 10:02:11 +0200

    pdo

--- vendor/dal/Dal.php
+++ vendor/dal/Dal.php
@@ -22,7 +22,7 @@ class Dal {
      */
     private function getPdo() {
         try {
-            $pdo = new \PDO('pgsql:dbname=ostim_development;host=localhost;',
+            $pdo = new \PDO('pgsql:dbname=b2argebilisim_development;host=localhost;',
                             'postgres',
                             '1Qaaal123');
             return $pdo;
